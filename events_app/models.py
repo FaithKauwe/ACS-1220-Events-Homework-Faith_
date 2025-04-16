3945c908dc9147bdcb374cdd07819e8a7509b63c
FaithKauwe <fkauwe@gmail.com>
2025-04-16 16:31:36 -0700

    models complete

--- events_app/models.py
+++ events_app/models.py
@@ -1,32 +1,37 @@
 """Create database models to represent tables."""
 from events_app import db
 from sqlalchemy.orm import backref
+from sqlalchemy import Enum
 
-# TODO: Create a model called `Guest` with the following fields:
-# - id: primary key
-# - name: String column
-# - email: String column
-# - phone: String column
-# - events_attending: relationship to "Event" table with a secondary table
+# join table for the many-to-many relationship
+guest_event_table = db.Table('guest_event_table',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'), primary_key=True)
+)
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    events_attending = db.relationship(
+        'Event', 
+        secondary=guest_event_table,
+        back_populates='guests'
+    )
 
-# TODO: Create a model called `Event` with the following fields:
-# - id: primary key
-# - title: String column
-# - description: String column
-# - date_and_time: DateTime column
-# - guests: relationship to "Guest" table with a secondary table
 
 # STRETCH CHALLENGE: Add a field `event_type` as an Enum column that denotes the
 # type of event (Party, Study, Networking, etc)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-# TODO: Create a table `guest_event_table` with the following columns:
-# - event_id: Integer column (foreign key)
-# - guest_id: Integer column (foreign key)
-
-guest_event_table = None
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
+    date_and_time = db.Column(db.DateTime, nullable=False)
+    guests = db.relationship(
+        'Guest',
+        secondary=guest_event_table,
+        back_populates='events_attending'
+    )
+    event_type = db.Column(Enum('Party', 'Study', 'Networking', 'Other', name='event_types'))
